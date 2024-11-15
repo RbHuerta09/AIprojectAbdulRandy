6e076f17909fc311bb1f76ac2190386b0cd47ab2
A-Issaka <abduli14@nycstudents.net>
2024-11-15 13:49:48 -0500

    sddsds

--- AR Project/AR Project/AR Project/script.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.querySelector('#myCanvas');
-const ctx = canvas.getContext('2d');
-
-ctx.beginPath();
-ctx.moveTo(0,0);
-ctx.lineTo(300,100);
-ctx.lineWidth = 5;
-ctx.stroke();
